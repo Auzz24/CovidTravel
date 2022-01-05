ec916cae46b7b75f75bf17f51e773d4cc207be8e
Austin Miller <austin.miller1424@gmail.com>
2022-01-05 15:12:48 -0600

    start of state vs state and country v country

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,4 @@
 var inputState = document.querySelector("#stateCode")
-var inputDestination = document.querySelector("#destination")
 var searchButton = document.querySelector("#searchButton")
 // Server Side API Response Website
 
@@ -12,7 +11,6 @@ function getData (state){
     })
     .then(response => {
         console.log(response);
-        var riskLevel = response.riskLevels.overall
         var totalCases = response.actuals.cases 
         var totalDeaths = response.actuals.deaths
         var positiveTests = response.actuals.positiveTests
@@ -34,9 +32,23 @@ searchButton.addEventListener("click", function(){
 getData(inputState.value)
 })
 
-function getFlightInfo (destination){
-    fetch ("https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/data/en-GB/airlines.json?apiKey=db93a64ddcmsh312cf56a679121dp127a3ejsne11329932c20")
-    .then(response => {
-        return response.json()
+var inputCountry = document.querySelector("#countryCode")
+var searchCountryButton = document.querySelector("#searchCounrtyButton")
+function getCountryInfo(country){
+    fetch ("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/northamerica", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
+            "x-rapidapi-key": "94c895e35bmsha5d3d63d53d272ap14d10cjsn4a1ba9717cc5"
+        }
     })
-}
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            console.log(response);
+	})
+};
+
+getCountryInfo ()
+
