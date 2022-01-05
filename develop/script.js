2bbbb96cfa183046777e569e3811ae98803ef32a
Austin Miller <austin.miller1424@gmail.com>
2022-01-05 16:36:15 -0600

    new java file, usa stats added

--- develop/script.js
+++ develop/script.js
@@ -32,23 +32,31 @@ searchButton.addEventListener("click", function(){
 getData(inputState.value)
 })
 
-var inputCountry = document.querySelector("#countryCode")
-var searchCountryButton = document.querySelector("#searchCounrtyButton")
-function getCountryInfo(country){
-    fetch ("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/northamerica", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
-            "x-rapidapi-key": "94c895e35bmsha5d3d63d53d272ap14d10cjsn4a1ba9717cc5"
-        }
+function getData2 (state){
+    fetch("https://api.covidactnow.org/v2/state/" + state + ".json?apiKey=09122ea58b9c408b9852a1b31c49f05a")
+
+    .then(response => {
+        return response.json()
     })
-        .then(response => {
-            return response.json()
-        })
-        .then(response => {
-            console.log(response);
-	})
-};
+    .then(response => {
+        console.log(response);
+        var totalCases = response.actuals.cases 
+        var totalDeaths = response.actuals.deaths
+        var positiveTests = response.actuals.positiveTests
+        var icuBedCapacity = response.actuals.icuBeds.capacity
+        var icuTotal = response.actuals.icuBeds.currentUsageTotal
 
-getCountryInfo ()
+        document.querySelector("#totalDeaths2").textContent = totalDeaths
+        document.querySelector("#totalCases2").textContent = totalCases
+        document.querySelector("#positiveTests2").textContent = positiveTests
+        document.querySelector("#icuCapacity2").textContent = icuBedCapacity
+        document.querySelector("#icuTotal2").textContent = icuTotal
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
 
+searchButton2.addEventListener("click", function(){
+getData2(inputState.value)
+})
